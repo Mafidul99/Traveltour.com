df5b13848c18d482214e0f52479133365c23c014
Mafidul99 <mdonline192@gmail.com>
2023-08-30 12:12:23 +0530

    all are update

--- common/tourbythemes/wildlife/enquiry-form.php
+++ common/tourbythemes/wildlife/enquiry-form.php
@@ -21,22 +21,7 @@
     <link rel="preload" as="script" href="../../../assets/js/catalog_new.js">
 
 
-    <div class="whChatSec pf dib c5px">
-        <div class="whHdSec">
-            <div class="whtCls"><img src="../../../assets/img/whs-close.png" alt="" width="16" height="16" loading="lazy" /></div>
-            <h4 class="xxlarge mb5px fw6 db lh13em">Raise your Query</h4>
-            <p>Hi! Simply click below and type your query.</p>
-        </div>
-        <div class="whChatCont">
-            <p class="small mb7px">Our experts will reply you very soon.</p>
-            <div class="whChatList">
-                <a class="pr cp db bsbb" href="https://api.whatsapp.com/send?phone=919864033984" target="_blank" rel="nofollow"> <span class="large db">Click Here to Chat </span> <span class="small">Souvenir Solutions</span></a>
-            </div>
-        </div>
-    </div>
-    <div class="whatsapp-sec pf">
-        <div class="whatsappBtn"><i class="fa fa-whatsapp"></i> <span class="whTxt">WhatsApp Us</span></div>
-    </div>
+    <?php include('../../../pages/WhatsApp_tourbytheme.php'); ?>
 </head>
 
 <body class="del_inHdr ">
